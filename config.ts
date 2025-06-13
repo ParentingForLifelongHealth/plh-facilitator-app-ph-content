122f6aac93da0216aca8d25200367cc3f2ddd3bc
Chris Marsh <chris.marsh@idems.international>
2025-06-13 16:05:37 +0100

    content: 1.2.10

--- config.ts
+++ config.ts
@@ -19,7 +19,7 @@ config.google_drive.assets_folder_ids = [
 
 config.git = {
   content_repo: "https://github.com/IDEMSInternational/plh-facilitator-app-ph-content.git",
-  content_tag_latest: "1.2.9",
+  content_tag_latest: "1.2.10",
 };
 
 config.android = {
