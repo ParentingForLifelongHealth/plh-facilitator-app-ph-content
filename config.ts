5aed610e3efa4a5454ae0b68d331404d9473a899
EsmeeIDEMS <esmee@idems.international>
2025-06-18 17:54:53 +0100

    content: 1.2.13

--- config.ts
+++ config.ts
@@ -19,7 +19,7 @@ config.google_drive.assets_folder_ids = [
 
 config.git = {
   content_repo: "https://github.com/IDEMSInternational/plh-facilitator-app-ph-content.git",
-  content_tag_latest: "1.2.12",
+  content_tag_latest: "1.2.13",
 };
 
 config.android = {
